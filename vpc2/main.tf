208e027d524acc568cad421f4bb53a250091a7d2
VKhodyrev <vkhodyrev1@gmail.com>
2022-06-27 15:12:50 +0300

    Go on tags correction

--- vpc2/main.tf
+++ vpc2/main.tf
@@ -3,11 +3,11 @@ resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name          = "terraform-train2-${var.infra_env}-vpc"
-    Project       = "terraform-train2.io"
-    Environment   = var.infra_env
-    ManagedBy     = "terraform"
-    var.tags_name = "${var.tags_value}"
+    # Project            = "terraform-train2.io"
+    # ManagedBy          = "terraform"
+    Name               = "terraform-train2-${var.infra_env}-vpc"
+    Environment        = var.infra_env
+    "${var.tags_name}" = "${var.tags_value}"
   }
 }
 
@@ -22,12 +22,13 @@ resource "aws_subnet" "public" {
   cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
 
   tags = {
-    Name        = "terraform-train2-${var.infra_env}-public-subnet"
-    Project     = "terraform-train2.io"
-    Role        = "public"
-    Environment = var.infra_env
-    ManagedBy   = "terraform"
-    Subnet      = "${each.key}-${each.value}"
+    # Project     = "terraform-train2.io"
+    # Role        = "public"
+    # ManagedBy   = "terraform"
+    Name               = "terraform-train2-${var.infra_env}-public-subnet"
+    Environment        = var.infra_env
+    Subnet             = "${each.key}-${each.value}"
+    "${var.tags_name}" = "${var.tags_value}"
   }
 }
 
@@ -42,11 +43,12 @@ resource "aws_subnet" "private" {
   cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, each.value)
 
   tags = {
-    Name        = "terraform-train2-${var.infra_env}-private-subnet"
-    Project     = "terraform-train2.io"
-    Role        = "private"
-    Environment = var.infra_env
-    ManagedBy   = "terraform"
-    Subnet      = "${each.key}-${each.value}"
+    # Project     = "terraform-train2.io"
+    # Role        = "private"
+    # ManagedBy   = "terraform"
+    Name               = "terraform-train2-${var.infra_env}-private-subnet"
+    Environment        = var.infra_env
+    Subnet             = "${each.key}-${each.value}"
+    "${var.tags_name}" = "${var.tags_value}"
   }
 }
